df0d613436464476eea7ce0acdd2ad6bd539284a
Toby Mackenzie <public@tobymackenzie.com>
2014-04-22 01:06:15 -0400

    *sh/commands: fancier git log commands

--- fish/commands/vcs.fish
+++ fish/commands/vcs.fish
@@ -2,9 +2,33 @@
 alias gitcommit="git diff --cached | less; and git commit"
 alias gitcommitall="git add .; and gitcommit"
 
+#--logging
 #-@ https://github.com/addyosmani/dotfiles/blob/master/.functions#L115
+#-@ https://coderwall.com/p/euwpig
+#---short
+function gitl
+	git log $argv --pretty=format:'%C(green)--%C(reset) %s' --date=short --no-merges
+end
+#---base for others
+set gitlogbase_format '%s %C(green)(%cd) %C(bold blue)<%an> %C(yellow)%h %C(red)%d%C(reset)'
+function gitlogbase
+	git log $argv --date=iso
+end
+#---default
 function gitlog
-	git log $argv --pretty=format:'-- (%ad:%an): %s' --date=short --no-merges
+	gitlogg $argv
+end
+#---with graph
+function gitlogg
+	gitlogbase $argv --graph --pretty=format:' '{$gitlogbase_format}
+end
+#---with names
+function gitlogn
+	gitlogbase $argv --name-only --pretty=format:'%C(green)--%C(reset) '{$gitlogbase_format}
+end
+#---with diff
+function gitlogp
+	gitlogbase $argv -p --pretty=format:'%C(red)--------------------------------------------------------%C(reset)%n'{$gitlogbase_format}'%n%C(red)--------------------------------------------------------%C(reset)%n'
 end
 
 #==svn
--- sh/commands/vcs.sh
+++ sh/commands/vcs.sh
@@ -2,10 +2,30 @@
 alias gitcommit="git diff --cached | less && git commit"
 alias gitcommitall="git add . && gitcommit"
 
+#--logging
 #-@ https://github.com/addyosmani/dotfiles/blob/master/.functions#L115
-function gitlog(){
-	git log $1 $2 --pretty=format:'-- (%ad:%an): %s' --date=short --no-merges
+#-@ https://coderwall.com/p/euwpig
+#---short
+function gitl(){
+	git log $@ --pretty=format:'%C(green)--%C(reset) %s' --date=short --no-merges
 }
+#---base for others
+gitlogbase_format="%s %C(green)(%cd) %C(bold blue)<%an> %C(yellow)%h %C(red)%d%C(reset)"
+#---default
+alias gitlog="gitlogg"
+#---with graph
+function gitlogg(){
+	git log $@ --date=iso --graph --pretty=format:" ${gitlogbase_format}"
+}
+#---with names
+function gitlogn(){
+	git log $@ --date=iso --name-only --pretty=format:"%C(green)--%C(reset) ${gitlogbase_format}"
+}
+#---with diff
+function gitlogp(){
+	git log $@ --date=iso -p --pretty=format:"%C(red)--------------------------------------------------------%C(reset)%n${gitlogbase_format}%n%C(red)--------------------------------------------------------%C(reset)%n"
+}
+
 
 #==svn
 alias svnstatus="svn status --ignore-externals"
