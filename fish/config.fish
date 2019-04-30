17463cb4ca78a725c96d977d082983814779d3e7
Toby Mackenzie <public@tobymackenzie.com>
2019-04-30 00:25:01 -0400

    fish: remove brackets around variables, fish doesn't like

--- fish/config.fish
+++ fish/config.fish
@@ -3,10 +3,10 @@ source ~/.dotfiles/fish/load.fish
 #--aliases
 #---directories
 function __
-	cd {$HOME}/__/{$argv}
+	cd "$HOME/__/$argv"
 end
-#alias ~="cd {$HOME}"
+#alias ~="cd $HOME"
 	#alias /="cd /"
 alias cdbin="cd ~/bin"
 alias bin="cdbin"
-alias cdhome="cd {$HOME}"
+alias cdhome="cd $HOME"
