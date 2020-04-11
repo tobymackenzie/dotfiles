c7831700b225cdfb7c2ba9afb12b3f0aae9354c4
Toby Mackenzie <public@tobymackenzie.com>
2020-04-10 23:28:16 -0400

    feat(prompt): improve appearance with background

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,9 +1,10 @@
 function fish_prompt
 	set_color white -b green
-	echo -n '[<'
+	echo -n '['
 	echo -n -s "$USER"
 	echo -n ':'
 	echo -n (basename $PWD)
-	echo -n '>] '
+	echo -n ']>'
 	set_color normal
+	echo -n ' '
 end
--- sh/config/prompt.sh
+++ sh/config/prompt.sh
@@ -1,8 +1,8 @@
 ##PS1="[<\t@\h:\W>] "
-PS1="[<\u:\W>] "
+PS1="[\u:\W]>"
 if [ -n "${TERM}" ]; then
 	tputFG="$(tput setab 2)$(tput setaf 7)"
 	tputReset="$(tput sgr0)"
 	PS1="${tputFG}${PS1}${tputReset}"
 fi
-export PS1=$PS1
+export PS1="$PS1 "
