9558e761c42d446cac131607144304f8d88904d5
Toby Mackenzie <public@tobymackenzie.com>
2021-03-18 15:43:08 -0400

    feat(shell): remove git commands made redundant by git aliases

--- fish/commands/vcs.fish
+++ fish/commands/vcs.fish
@@ -1,22 +1,5 @@
 #==git
 alias g='git'
-alias gitcommit='git c'
-alias gitcommitall='git call'
-
-#--count
-alias gitcount='git count'
-
-#--logging
-#---short
-alias gitl='git l'
-#---default
-alias gitlog='git lg'
-#---with graph
-alias gitlogg='git lg'
-#---with names
-alias gitlogn='git logn'
-#---with diff
-alias gitlogp='git lop'
 
 #==svn
 alias svnstatus="svn status --ignore-externals"
