239cef559681163697ba7766e18ec1d420bcfa16
Toby Mackenzie <public@tobymackenzie.com>
2021-08-01 22:38:39 -0400

    fix(bash/zsh): prompt was breaking when typing long commands, navigating command, etc

--- sh/config/prompt.sh
+++ sh/config/prompt.sh
@@ -3,11 +3,11 @@ PS1="[\u:\W]>"
 tputColors="$(tput colors 2> /dev/null || echo 2)"
 if [ $tputColors -gt 2 ]; then
 	if [ $tputColors -gt 8 ]; then
-		tputFG="$(tput setab 42)$(tput setaf 0)"
+		tputFG="\[$(tput setab 42)$(tput setaf 0)\]"
 	else
-		tputFG="$(tput setab 2)$(tput setaf 7)"
+		tputFG="\[$(tput setab 2)$(tput setaf 7)\]"
 	fi
-	tputReset="$(tput sgr0)"
+	tputReset="\[$(tput sgr0)\]"
 	PS1="${tputFG}${PS1}${tputReset}"
 fi
 export PS1="$PS1 "
