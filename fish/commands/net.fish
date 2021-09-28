6923327368564480e1d6ba441dd4847a0d682a05
Toby Mackenzie <public@tobymackenzie.com>
2021-09-28 19:58:02 -0400

    feat(sh): add `ssha` alias for quicker port forwarding

--- fish/commands/net.fish
+++ fish/commands/net.fish
@@ -1,6 +1,7 @@
 if [ $TJM_OS = 'darwin' ]
 	alias cleardns="dscacheutil -flushcache"
 end
+alias ssha="ssh -A"
 function whoisl
 	whois $argv | less
 end
--- sh/commands/net.sh
+++ sh/commands/net.sh
@@ -1,6 +1,7 @@
 if [[ "${TJM_OS}" == 'darwin' ]]; then
 	alias cleardns="dscacheutil -flushcache"
 fi
+alias ssha="ssh -A"
 whoisl(){
 	whois $* | less
 }
