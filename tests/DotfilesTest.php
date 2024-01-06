2133854d7fd27767f0d9728482e18cf9a870f2cf
Toby Mackenzie <public@tobymackenzie.com>
2024-01-05 20:59:15 -0500

    test: move dir creation removal to setUp / tearDown so they happen even with errors

--- tests/DotfilesTest.php
+++ tests/DotfilesTest.php
@@ -5,16 +5,21 @@ use PHPUnit\Framework\TestCase;
 require_once(__DIR__ . '/../src/Dotfiles.php');
 
 class DotfilesTest extends TestCase{
+	const DIR = __DIR__ . '/tmp';
+	public function setUp(): void{
+		mkdir(self::DIR);
+	}
+	public function tearDown(): void{
+		shell_exec("rm -rf " . self::DIR);
+	}
 	public function testFilePlacement(){
-		$dir = __DIR__ . '/tmp';
-		mkdir($dir);
 		$dotfiles = new Dotfiles(array(
-			'homeDir'=> $dir,
+			'homeDir'=> self::DIR,
 			'interactive'=> false,
 			'verbose'=> false,
 		));
 		$dotfiles->placeAllFiles();
-		$existingPaths = explode("\n", trim(shell_exec("find {$dir} -type l -or -type f")));
+		$existingPaths = explode("\n", trim(shell_exec("find " . self::DIR . " -type l -or -type f")));
 		foreach(array(
 			'.atom',
 			'.bash_profile',
@@ -31,7 +36,7 @@ class DotfilesTest extends TestCase{
 			'.zshenv',
 			'.zshrc',
 		) as $testPath){
-			$testPath = $dir . '/' . $testPath;
+			$testPath = self::DIR . '/' . $testPath;
 			$this->assertContains($testPath, $existingPaths, "File {$testPath} should have been created.");
 			$key = array_search($testPath, $existingPaths);
 			if($key !== false){
@@ -39,6 +44,5 @@ class DotfilesTest extends TestCase{
 			}
 		}
 		$this->assertEmpty($existingPaths, 'There should be no extra files found in destination folder, ' . count($existingPaths) . ' found: ' . json_encode($existingPaths));
-		shell_exec("rm -rf {$dir}");
 	}
 }
