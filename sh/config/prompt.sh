ec52590acab641a2def70242e07f80f16a684fbf
Toby Mackenzie <public@tobymackenzie.com>
2024-01-07 00:51:34 -0500

    code(shells): get rid of some unneeded global variables set for prompt

--- sh/config/prompt.sh
+++ sh/config/prompt.sh
@@ -3,13 +3,13 @@ PS1="[\u:\W]>"
 tputColors="$(tput colors 2> /dev/null || echo 2)"
 if [ $tputColors -gt 2 ]; then
 	if [ $tputColors -gt 8 ]; then
-		tputFG="\[$(tput setab 42)$(tput setaf 0)\]"
+		PS1="\[$(tput setab 42)$(tput setaf 0)\]${PS1}"
 	else
-		tputFG="\[$(tput setab 2)$(tput setaf 7)\]"
+		PS1="\[$(tput setab 2)$(tput setaf 7)\]${PS1}"
 	fi
-	tputReset="\[$(tput sgr0)\]"
-	PS1="${tputFG}${PS1}${tputReset}"
+	PS1+="\[$(tput sgr0)\]"
 fi
+unset -v tputColors
 #-# PS0 shows timestamp before command in BASH>=4.4
 export PS0="@ \$(date '+%H:%M:%S %Y-%m-%d')\n"
 export PS1="@ \$(date '+%H:%M:%S %Y-%m-%d')\n$PS1 "
