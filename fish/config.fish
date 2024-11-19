c896b20fbdf35e7bb661c0bb50f3b5025aa98161
Toby Mackenzie <public@tobymackenzie.com>
2024-11-19 16:55:46 -0500

    fix(shells): `reshell` was doing login shell rather than current shell

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,10 @@
+#--store shell name for `reshell`
+set TJM_SHELL fish
+
+#--set dotfiles path
 if test -z "$TJM_DOTFILES_PATH"
 	set -x TJM_DOTFILES_PATH (dirname (dirname (readlink -f (status -f))))
 end
+
+#--load all conf
 source {$TJM_DOTFILES_PATH}/fish/load.fish
--- shells/alias.sh
+++ shells/alias.sh
@@ -4,7 +4,7 @@ alias df="df -h"
 alias du="du -h"
 alias lynx="lynx -cfg ~/.dotfiles/lynx/lynx.cfg"
 alias mv="mv -i"
-alias reshell="exec $SHELL"
+alias reshell="exec $TJM_SHELL"
 alias scp="scp -pr"
 alias w3m="w3m -config ~/.dotfiles/w3m/config"
 
