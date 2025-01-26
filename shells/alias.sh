f5ca661757c7327406b77ff26c88bf6391c4d021
Toby Mackenzie <public@tobymackenzie.com>
2025-01-26 14:16:15 -0500

    code(shell): organize in sections

--- shells/alias.sh
+++ shells/alias.sh
@@ -1,13 +1,13 @@
 ##--hold general aliases that don't fit elsewhere
+alias reshell="exec $TJM_SHELL"
+alias tputshow='tputcolor'
+
+#--better defaults
 alias cp="cp -pri"
 alias df="df -h"
 alias du="du -h"
-alias lynx="lynx -cfg ~/.dotfiles/lynx/lynx.cfg"
 alias mv="mv -i"
-alias reshell="exec $TJM_SHELL"
 alias scp="scp -pr"
-alias tputshow='tputcolor'
-alias w3m="w3m -config ~/.dotfiles/w3m/config"
 
 #--merge options to command for quicker typing, autocomplete
 alias grepr="grep -R"
@@ -15,4 +15,8 @@ alias grepri="grep -Ri"
 alias greprl="grep -Rl"
 alias greprli="grep -Rli"
 alias typea="type -a"
+
+#--custom config path
+alias lynx="lynx -cfg ~/.dotfiles/lynx/lynx.cfg"
+alias w3m="w3m -config ~/.dotfiles/w3m/config"
 
