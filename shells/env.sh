486f9c41bae81f499f4075d51e10936a3f31ec0a
Toby Mackenzie <public@tobymackenzie.com>
2025-02-16 17:01:56 -0500

    feat(bin): add 'bugh' command to back up a github account's repos

--- shells/env.sh
+++ shells/env.sh
@@ -2,6 +2,7 @@
 setdefaultenv TJM_DOTFILES_PATH ~/.dotfiles
 setdefaultenv TJM_MAIN_PATH ~/__
 setdefaultenv TJM_BIN_PATH ~/bin
+setdefaultenv TJM_GH_BU_PATH ~/bu/github
 setdefaultenv TJM_PROJ_PATH $TJM_MAIN_PATH/checkouts
 setdefaultenv TJM_CHECKOUTS_PATH $TJM_PROJ_PATH
 
