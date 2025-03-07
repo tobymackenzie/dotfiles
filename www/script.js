79949abb9f5c1a357496e52cad5e910ae21b7dad
Toby Mackenzie <public@tobymackenzie.com>
2025-03-07 16:31:48 -0500

    whimsy(www): better konami code action

--- www/script.js
+++ www/script.js
@@ -1,12 +1,17 @@
 (function(){
-if(document.addEventListener){
+if(document.addEventListener && document.querySelector){
 	var _current = 0;
 	var _keys = [38,38,40,40,37,39,37,39,66,65];
 	document.addEventListener('keyup', function(_event){
 		var _key =  _event.which || _event.keyCode;
 		if(_key === _keys[_current]){
 			if(_current === 9){
-				window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+				var d = document.querySelector('html').style;
+				if(!d.transform){
+					d.transform = 'rotate(180deg)';
+				}else{
+					d.transform = '';
+				}
 			}else{
 				++_current;
 			}
