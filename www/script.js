d4510c5a73f5aeed1aa63f1a907817f7ecd5b8c1
Toby Mackenzie <public@tobymackenzie.com>
2025-07-18 10:04:48 -0400

    feat(www): update reddit links to JS friendly site for JS browsers

--- www/script.js
+++ www/script.js
@@ -1,4 +1,5 @@
 (function(){
+//--easter egg
 if(document.addEventListener && document.querySelector){
 	var _current = 0;
 	var _keys = [38,38,40,40,37,39,37,39,66,65];
@@ -21,5 +22,15 @@ if(document.addEventListener && document.querySelector){
 			}
 		}
 	});
+}
+//--adjust links for JS browsers
+if(document.querySelectorAll){
+	var links = document.querySelectorAll('a[href*="/old.reddit.com"]');
+	if(links){
+		for(var i = 0; i < links.length; ++i){
+			var link = links[i];
+			link.href = link.href.replace('/old.', '/www.');
+		}
+	}
 }
 })();
