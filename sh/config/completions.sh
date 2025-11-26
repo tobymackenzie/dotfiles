25db09c1a44e26f1d88343d4082a47c87b0aec84
Toby Mackenzie <public@tobymackenzie.com>
2025-11-26 15:33:18 -0500

    fix(bash): completion ignore case setting was erroring in non-interactive shell

--- sh/config/completions.sh
+++ sh/config/completions.sh
@@ -1,4 +1,4 @@
 #--case insensitive completions
-if [ -n "$BASH_VERSION" ] && type caller >/dev/null 2>&1; then
+if [ -n "$BASH_VERSION" ] && [[ $- == *-i* ]] && type caller >/dev/null 2>&1; then
 	bind 'set completion-ignore-case on'
 fi
