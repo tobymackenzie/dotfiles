09c9feb38877ab6e39bfbd19f9328f458d2202ad
Toby Mackenzie <public@tobymackenzie.com>
2026-02-19 15:00:52 -0500

    feat(shell): better `top` defaults for mac

--- shells/mac.sh
+++ shells/mac.sh
@@ -7,8 +7,9 @@ alias macvim="oa MacVim"
 alias cleardns="dscacheutil -flushcache"
 
 #==system monitoring
-alias tcpu="top -S -F -ocpu -s 2 -stats pid,command,cpu,user,time,mem,threads,state"
-alias tmem='top -S -orsize -s 5 -stats pid,command,mem,purg,cmprs,user,cpu,time,threads,state'
-alias tt="top -S -n 0 -s 5"
-alias ttime='top -F -otime -s 5 -stats pid,command,time,cpu,user,mem,threads,state'
+alias top='top -F -s 2 -S -ocpu -stats pid,command,user,cpu,time,mem,state,threads,cpu_me,ppid'
+alias tcpu="\\top -S -F -ocpu -s 2 -stats pid,command,cpu,user,time,mem,threads,state"
+alias tmem='\top -S -orsize -s 5 -stats pid,command,mem,purg,cmprs,user,cpu,time,threads,state'
+alias tt="\\top -S -n 0 -s 5"
+alias ttime='\top -F -otime -s 5 -stats pid,command,time,cpu,user,mem,threads,state'
 
